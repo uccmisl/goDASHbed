ddcd91df141eea13cc904cf4d94002f26ad30eed
Jason Quinlan <j.quinlan@cs.ucc.ie>
2020-05-27 17:56:07 +0100

    update

--- hypercorn_goDASHbed.py
+++ hypercorn_goDASHbed.py
@@ -6,49 +6,67 @@ from quart import send_from_directory
 from quart_trio import QuartTrio
 from os import path
 
+from argparse import ArgumentParser
+
 # this defines the root folder containing our DASH dataset
 dash_content_path = '/var/www/html/'
 
-# define the config setup for our testbed
-config = Config()
-# config.quic_bind = ["10.0.0.1:443"]  # port number to use for HTTP
-# config.bind = ["10.0.0.1:4444"]  # port number to use for HTTPS
-# config.insecure_bind = ["100.168.1.15:2222"]  # port number to use for QUIC
 
-# locations for the cert and key
-config.certfile = "../goDASH/godash/http/certs/cert.pem"
-config.keyfile = "../goDASH/godash/http/certs/key.pem"
+def create_app(use_quic=False):
+    # this 'root_path' is needed by QuartTrio to point to the DASH video content folder
+    app = QuartTrio(__name__, root_path=dash_content_path)
 
-# this 'root_path' is needed by QuartTrio to point to the DASH video content folder
-app = QuartTrio(__name__, root_path=dash_content_path)
+    # define the config setup for our testbed
+    config = Config()
+    if use_quic:
+        return
+    # config.quic_bind = ["10.0.0.1:443"]  # port number to use for HTTP
+    # config.bind = ["10.0.0.1:4444"]  # port number to use for HTTPS
+    # config.insecure_bind = ["100.168.1.15:2222"]  # port number to use for QUIC
 
-# return 404, if file not found
-@app.errorhandler(404)
-# @app.route('/')
-async def page_not_found(error):
-    return ' File not found', 404
+    # locations for the cert and key
+    config.certfile = "../goDASH/godash/http/certs/cert.pem"
+    config.keyfile = "../goDASH/godash/http/certs/key.pem"
 
-# this return index.html if nothing is added to the url and port
-@app.route('/')
-async def root():
+    # return 404, if file not found
+    @app.errorhandler(404)
+    # @app.route('/')
+    async def page_not_found(error):
+        return ' File not found', 404
 
-    print("returning index.html",)
-    return await send_from_directory(dash_content_path, 'index.html'), 200
+    # this return index.html if nothing is added to the url and port
+    @app.route('/')
+    async def root():
+
+        print("returning index.html",)
+        return await send_from_directory(dash_content_path, 'index.html'), 200
+
+    # this return a file if a path is added after the url and port
+    @app.route('/<path:path_to_DASH_files>')
+    async def index(path_to_DASH_files=dash_content_path):
+
+        # if the file does not exist, return 404
+        path_to_file = path.join(dash_content_path, path_to_DASH_files)
+        if not path.isfile(path_to_file):
+            print("This file does not exist:", path.isfile(path_to_file))
+            return path_to_file + ' : File not found', 404
+
+        # we need the await or we get coroutine error
+        print("File downloaded", path_to_file)
+        return await send_from_directory(dash_content_path, path_to_DASH_files), 200
+
+    return app, config
 
 
-# this return a file if a path is added after the url and port
-@app.route('/<path:path_to_DASH_files>')
-async def index(path_to_DASH_files=dash_content_path):
-
-    # if the file does not exist, return 404
-    path_to_file = path.join(dash_content_path, path_to_DASH_files)
-    if not path.isfile(path_to_file):
-        print("This file does not exist:", path.isfile(path_to_file))
-        return path_to_file + ' : File not found', 404
-
-    # we need the await or we get coroutine error
-    print("File downloaded", path_to_file)
-    return await send_from_directory(dash_content_path, path_to_DASH_files), 200
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-quic')
+    args = parser.parse_args()
+    use_quic = args.quic
+    app, config = create_app(use_quic)
+    # use trio to get our files
+    trio.run(partial(serve, app, config))
 
 # use trio to get our files
-trio.run(partial(serve, app, config))
+# trio.run(partial(serve, app, config))
