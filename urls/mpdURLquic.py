3577adc7ee40921d579bcd37020c3d6270128be6
jq5 <jq5@users.noreply.github.com>
2020-08-01 17:52:01 +0100

    new quic url file

---
+++ urls/mpdURLquic.py
@@ -0,0 +1,14 @@
+full_url_list_quic = [
+    "https://www.godashbed.org:4444/4K_dataset/4_sec/x264/bbb/DASH_Files/full/bbb_enc_x264_dash.mpd"]
+
+main_url_list_quic = [
+    "https://www.godashbed.org:4444/4K_dataset/4_sec/x264/bbb/DASH_Files/full/bbb_enc_x264_dash.mpd"]
+
+live_url_list_quic = [
+    "https://www.godashbed.org:4444/4K_dataset/4_sec/x264/bbb/DASH_Files/full/bbb_enc_x264_dash.mpd"]
+
+full_byte_range_url_list_quic = [
+    "https://www.godashbed.org:4444/4K_dataset/4_sec/x264/bbb/DASH_Files/full/bbb_enc_x264_dash.mpd"]
+
+main_byte_range_url_list_quic = [
+    "https://www.godashbed.org:4444/4K_dataset/4_sec/x264/bbb/DASH_Files/full/bbb_enc_x264_dash.mpd"]
